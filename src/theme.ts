29cf3637637e7e4e6f164e9f98f33aa03c09dde8
UHquokka <bananamayong@gmail.com>
2023-05-03 21:54:28 +0900

    #6.10 react-form-hook 으로 todo input 만들기

---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from "styled-components";
+
+export const lightTheme: DefaultTheme = {
+  bgColor: "whitesmoke",
+  textColor: "black",
+  accentColor: "#10ac84",
+};
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "black",
+  textColor: "black",
+  accentColor: "#ff9ff3",
+};
