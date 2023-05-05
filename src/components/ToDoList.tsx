07413110db36611048bc4b191949025fa66122a8
UHquokka <bananamayong@gmail.com>
2023-05-05 17:49:32 +0900

    selector 을 이용해 category 별로 state 받아오기

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,20 +1,32 @@
 import React, { useState } from "react";
 import { useRecoilValue } from "recoil";
 import CreateToDo from "./CreateToDo";
-import { toDoState } from "../atoms";
+import { toDoSelector, toDoState } from "../atoms";
 import ToDo from "./ToDo";
 
 export default function ToDoList() {
-  const toDos = useRecoilValue(toDoState);
+  const [toDo, doing, done] = useRecoilValue(toDoSelector);
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
       <CreateToDo />
       <ul>
-        {toDos.map((todo) => (
+        <h1>To Do</h1>
+        {toDo.map((todo) => (
           <ToDo key={todo.id} {...todo} />
         ))}
+        <hr />
+        <h1>Doing</h1>
+        {doing.map((todo) => (
+          <ToDo key={todo.id} {...todo} />
+        ))}
+        <hr />
+        <h1>Done</h1>
+        {done.map((todo) => (
+          <ToDo key={todo.id} {...todo} />
+        ))}
+        <hr />
       </ul>
     </div>
   );
