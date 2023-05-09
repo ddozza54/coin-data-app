fa0f637f7970ff8e96e4e44ed32eb65b3a61d4a2
UHquokka <bananamayong@gmail.com>
2023-05-09 20:55:39 +0900

    react-beautiful-dnd 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,7 @@
-import { useRecoilState, useRecoilValue } from "recoil";
-import { hourSelector, minuteState } from "./atoms";
-import React from "react";
+import { DragDropContext } from "react-beautiful-dnd";
 
 function App() {
-  const [minutes, setMinutes] = useRecoilState(minuteState);
-  const [hours, setHours] = useRecoilState(hourSelector);
-  const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
-    setMinutes(+event.currentTarget.value);
-  };
-  const onHoursChange = (event: React.FormEvent<HTMLInputElement>) => {
-    setHours(+event.currentTarget.value);
-  };
-  return (
-    <div>
-      <input
-        value={minutes}
-        onChange={onMinutesChange}
-        type="number"
-        placeholder="Minites"
-      />
-      <input
-        onChange={onHoursChange}
-        value={hours}
-        type="number"
-        placeholder="Hours"
-      />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
