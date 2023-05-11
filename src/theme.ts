eb8b6f019f9c55795c1955bb51a1c3dfe99825b1
UHquokka <bananamayong@gmail.com>
2023-05-11 13:02:20 +0900

    DragDropContext, Droppable, Draggable 을 이용한 list

--- src/theme.ts
+++ src/theme.ts
@@ -1,13 +1,19 @@
 import { DefaultTheme } from "styled-components";
 
-export const lightTheme: DefaultTheme = {
-  bgColor: "whitesmoke",
-  textColor: "black",
-  accentColor: "#10ac84",
-};
+// export const lightTheme: DefaultTheme = {
+//   bgColor: "whitesmoke",
+//   textColor: "black",
+//   accentColor: "#10ac84",
+// };
 
-export const darkTheme: DefaultTheme = {
-  bgColor: "black",
-  textColor: "black",
-  accentColor: "#ff9ff3",
+// export const darkTheme: DefaultTheme = {
+//   bgColor: "black",
+//   textColor: "black",
+//   accentColor: "#ff9ff3",
+// };
+
+export const theme: DefaultTheme = {
+  bgColor: "#3F8CF2",
+  boardColor: "#DADFE9",
+  cardColor: "white",
 };
