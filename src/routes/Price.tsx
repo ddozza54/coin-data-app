ed3a49d4b7564824d10c37bafd3ddd3d74480510
UHquokka <bananamayong@gmail.com>
2023-05-12 22:57:07 +0900

    Price tab 만들기

--- src/routes/Price.tsx
+++ src/routes/Price.tsx
@@ -5,24 +5,62 @@ import styled from "styled-components";
 
 const Wrapper = styled.div`
   width: 100%;
-  background-color: rosybrown;
-  height: 100%;
+  height: 300px;
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   grid-template-rows: repeat(3, 1fr);
-  gap: 5px;
-`;
-
-const DataBox = styled.div`
-  background-color: whitesmoke;
-  border-radius: 10px;
+  gap: 10px;
+  font-size: 18px;
+  h4 {
+    color: ${(prop) => prop.theme.accentColor};
+  }
 `;
 
 const Volume = styled.div`
   grid-column: 1/ -1;
   grid-row: 1/ 2;
-  background-color: whitesmoke;
+  background-color: ${(prop) => prop.theme.tabColor};
   border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  padding: 5px;
+  span {
+    font-weight: 600;
+  }
+`;
+
+const HighestPrice = styled.div`
+  grid-template-columns: 1/2;
+  grid-template-rows: 2/3;
+  background-color: ${(prop) => prop.theme.tabColor};
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  padding: 5px;
+  span {
+    color: tomato;
+    font-weight: 600;
+  }
+`;
+
+const LowesttPrice = styled.div`
+  grid-template-columns: 1/2;
+  grid-template-rows: 2/3;
+  background-color: ${(prop) => prop.theme.tabColor};
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  padding: 5px;
+  span {
+    color: skyblue;
+    font-weight: 600;
+  }
 `;
 
 interface PriceProps {
@@ -44,15 +82,41 @@ export default function Price({ coinId }: PriceProps) {
   const { isLoading, data } = useQuery<IHistorical[]>(["price", coinId], () =>
     fetchCoinHistory(coinId)
   );
+  const volumes = data?.map((v) => {
+    return Number(v.volume).toFixed(2);
+  });
+  const volume = volumes?.pop();
 
   const high = data?.map((v) => {
     return v.high;
   });
+  const highest = high?.pop();
+
+  const low = data?.map((v) => {
+    return v.low;
+  });
+  const lowest = low?.pop();
+
   return (
     <Wrapper>
-      <Volume>
-        <h1></h1>
-      </Volume>
+      {isLoading ? (
+        "Loading..."
+      ) : (
+        <>
+          <Volume>
+            <h4>오늘의 거래량</h4>
+            <span>{volume}</span>
+          </Volume>
+          <HighestPrice>
+            <h4>최고가</h4>
+            <span>$ {highest}</span>
+          </HighestPrice>
+          <LowesttPrice>
+            <h4>최저가</h4>
+            <span>$ {lowest}</span>
+          </LowesttPrice>
+        </>
+      )}
     </Wrapper>
   );
 }
