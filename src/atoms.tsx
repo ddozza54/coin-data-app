d3f448ff8f2d297865a8b7bd23fbf41b92a4c6b6
UHquokka <bananamayong@gmail.com>
2023-05-13 16:50:45 +0900

    State 수정 - Board 3개로 만들기

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -17,7 +17,15 @@ import { atom, selector } from "recoil";
 //   },
 // });
 
-export const toDoState = atom({
+interface IToDoState {
+  [key: string]: string[];
+}
+
+export const toDoState = atom<IToDoState>({
   key: "toDo",
-  default: ["a", "b", "c", "d", "e", "f"],
+  default: {
+    to_do: ["c", "e", "f"],
+    doing: ["a", "b"],
+    done: ["d"],
+  },
 });
